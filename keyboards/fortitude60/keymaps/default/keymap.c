8a0afbe9c25ae96670ac9a04a2e1cf92151c0e4a
Drew Myers <acmyers@us.ibm.com>
2021-01-11 15:17:48 -0500

    swap space and raise/lower keys

--- keyboards/fortitude60/keymaps/default/keymap.c
+++ keyboards/fortitude60/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  GUI |  Alt |Lower |Space | Ctrl |Ctrl  |Space |Raise |  Alt |  GUI |
+ *               |  GUI | Ctrl |Space |Lower |  Alt | Alt  |Raise |Space | Ctrl |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                 KC_LGUI, KC_LALT,    LOWER, KC_SPC, KC_LCTL,  KC_RCTL, KC_SPC, RAISE,    KC_RALT, KC_RGUI\
+                 KC_LGUI, KC_LCTL,    KC_SPC, LOWER, KC_LALT,  KC_RALT, RAISE, KC_SPC,    KC_RCTL, KC_RGUI\
 ),
 
 /* Colemak
